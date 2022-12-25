f8ed86508bff2d3d8a35eea4bf340ae7ed7e619c
Andrew Boshra <andrew.boshr@gmail.com>
2022-12-25 03:58:39 +0200

    reservation status for all

--- routes/reservation.js
+++ routes/reservation.js
@@ -1,10 +1,10 @@
 const controller = require("../controllers/reservation-controller");
 const authMiddleware = require("../middlewares/auth");
-const router = require("express").Router({mergeParams:true});
+const router = require("express").Router({ mergeParams: true });
 
 router
-    .use(authMiddleware.isAuthenticated(["Manager", "Fan"]))
     .get("/status", controller.status)
+    .use(authMiddleware.isAuthenticated(["Manager", "Fan"]))
     .get("/", controller.getAll)
     .post("/", controller.createReservationPayment)
     .post("/capture-payment", controller.captureReservation)
